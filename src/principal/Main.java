4c89a7807323cb480252016736c3509f5f8f1c07
jeasiel <jeasielabner@gmail.com>
2024-09-18 10:16:01 -0300

    reset

--- src/principal/Main.java
+++
@@ -1,8 +0,0 @@
-package principal;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Organizador main = new Organizador();
-        main.menuDaLista(null);
-    }
-}
