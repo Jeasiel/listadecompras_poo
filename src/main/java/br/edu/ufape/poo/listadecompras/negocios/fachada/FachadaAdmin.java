e5a4368cacaf18a21c8911ee93ceae24b7a674c0
jeasiel <jeasielabner@gmail.com>
2024-09-21 15:38:02 -0300

    verConta na fachada do usuario e limpeza da fachada do admin

--- src/main/java/br/edu/ufape/poo/listadecompras/negocios/fachada/FachadaAdmin.java
+++ src/main/java/br/edu/ufape/poo/listadecompras/negocios/fachada/FachadaAdmin.java
@@ -1,69 +1,17 @@
 package br.edu.ufape.poo.listadecompras.negocios.fachada;
 
-import java.util.ArrayList;
-
-import br.edu.ufape.poo.listadecompras.negocios.entidade.Conta;
+import br.edu.ufape.poo.listadecompras.negocios.cadastro.InterfaceCadastroConta;
+import br.edu.ufape.poo.listadecompras.negocios.cadastro.InterfaceCadastroLista;
+import br.edu.ufape.poo.listadecompras.negocios.cadastro.InterfaceCadastroProduto;
 
 public class FachadaAdmin {
     
-	private final ArrayList<Conta> listaContas = new ArrayList<>();
-    
-    public void gerenciarContas(){
-        //controler?
-    }
+	private InterfaceCadastroConta cadastroConta;
+    private InterfaceCadastroLista cadastroLista;
+    private InterfaceCadastroProduto cadastroProduto;
 
-    public void gerenciarListasGlobais(){
-        //ver como funcionaria
-    }
+	//Gerencia contas | CRUD
+
+	//Gerencia listas (com listas modelo) | CRUD
 	
-	public void adicionarConta(Conta u) {
-		//em construção
-		listaContas.add(u);
-	}
-
-	public void removerConta(Conta u) {
-		//Em construção
-		
-		if(consultarConta(u) == -1) {
-			//throwException
-		}
-		else {
-			listaContas.remove(consultarConta(u)); //remove com base no index dado;
-		}		
-	}
-	
-	public void atualizarConta(Conta u, String novoNome, String novoEmail, String novaSenha) {
-		//Em construção
-		
-		if(consultarConta(u) == -1) {
-			//throwException
-		}
-		else {
-			//Conta e = listaContas.get(consultarConta(u)); //retorna o index da lista correta
-			//e.editarConta(novoNome, novoEmail, novaSenha);
-		}	
-	}
-
-	public int consultarConta(Conta u) {
-		//Em construção
-		int index = listaContas.indexOf(u);
-
-		if(index >= 0) {
-			return index;
-		}
-		return -1;	
-	}
-
-	public ArrayList<Conta> getListaContas() {
-		//Em construção
-		
-		return listaContas;
-	}
-
-	public void setListaContas(ArrayList<Conta> listaContas) {
-		//Em construção
-		
-		this.listaContas.clear();
-		this.listaContas.addAll(listaContas);
-	}
 }
